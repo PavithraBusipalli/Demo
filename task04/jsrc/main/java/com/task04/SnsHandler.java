c246775be0fb6c9e88fac9a4154e835b869bf7d0
PavithraBusipalli <busipallipavitra@gmail.com>
2024-08-07 16:23:09 +0530

    task-04-0111

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -14,26 +14,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "sns_handler",
-		roleName = "sns_handler-role",
-		isPublishVersion = false,
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+       roleName = "sns_handler-role",
+       isPublishVersion = false,
+       logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(
-		targetTopic = "lambda_topic"
+       targetTopic = "lambda_topic"
 )
 @DependsOn(
-		name = "lambda_topic",
-		resourceType = ResourceType.SNS_TOPIC
+       name = "lambda_topic",
+       resourceType = ResourceType.SNS_TOPIC
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		LambdaLogger lambdaLogger = context.getLogger();
-		lambdaLogger.log(request.toString());
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
-	}
+    public Map<String, Object> handleRequest(Object request, Context context) {
+       LambdaLogger lambdaLogger = context.getLogger();
+       lambdaLogger.log(request.toString());
+       System.out.println("Hello from lambda");
+       Map<String, Object> resultMap = new HashMap<String, Object>();
+       resultMap.put("statusCode", 200);
+       resultMap.put("body", "Hello from Lambda");
+       return resultMap;
+    }
 }
